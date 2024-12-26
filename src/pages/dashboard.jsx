3a90369937836f03fef46f8d3f855596e3dc3b04
Nabila Lailatanzila <nabila5des@gmail.com>
2024-12-26 19:23:47 +0700

    logo gugel

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -79,10 +79,12 @@ const Dashboard = () => {
           </CardContent>
         </Card>
       </div>
+
       <div className="flex flex-col sm:flex-row justify-between items-center gap-4">
         <h1 className="text-2xl sm:text-4xl font-extrabold">My Links</h1>
         <CreateLink onNewUrl={handleNewUrl} />
       </div>
+
       <div className="relative">
         <Input
           type="text"
@@ -92,6 +94,7 @@ const Dashboard = () => {
         />
         <Filter className="absolute top-2 right-2 p-1" />
       </div>
+      
       {error && <Error message={error?.message} />}
       {(filteredUrls || []).map((url, i) => (
         <LinkCard key={i} url={url} fetchUrls={fnUrls} />
